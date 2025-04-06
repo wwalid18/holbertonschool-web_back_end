58b72c3a6c532e037fc0dac82bebb36e649afbe1
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-06 15:33:52 +0100

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => new Error());
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
