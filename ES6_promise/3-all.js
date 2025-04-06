119861e9a9ec3e292a41bdc385029c8fc3b975be
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-06 16:14:32 +0100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.body.firstName} ${userResponse.body.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
