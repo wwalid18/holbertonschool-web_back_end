6cd48060f9bfe21bc5fd155271bef6a49abb2b2d
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-12 13:35:27 +0100

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,59 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    // Type validation
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency object');
+    }
+
+    // Store in underscore attributes
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency object');
+    }
+    this._currency = value;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
