3f4d2307e8fb049da63436d1327cdb1d60130a60
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-12 13:20:24 +0100

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  const results = await Promise.allSettled(promises);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
