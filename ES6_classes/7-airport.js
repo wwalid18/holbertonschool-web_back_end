3d4303de3b635a1c2aff2fb50a76cb28f1421b14
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-12 13:44:36 +0100

    task 7

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,20 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    // Type validation
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
-    // Store in underscore attributes
     this._name = name;
     this._code = code;
   }
 
-  // Default string description
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
