6eff7c85e1c97bd2c1a377bddb19c4f851132285
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-26 15:14:25 +0100

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
-"""
-Module that defines the wait_n coroutine to spawn multiple wait_random coroutines
-and return their delays in ascending order.
-"""
+"""Module that defines an async routine to spawn multiple wait_random coroutines."""
 
 import asyncio
 from typing import List
-from 0_basic_async_syntax import wait_random
-
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns n wait_random coroutines and returns their delays in ascending order."""
-    delays = [wait_random(max_delay) for _ in range(n)]
-    results = []
-    
-    # Await all coroutines concurrently and add their results to results list
-    for delay in asyncio.as_completed(delays):
-        result = await delay
-        results.append(result)
+    """Spawns wait_random n times with the specified max_delay and returns the list of all delays in ascending order."""
+    delays = []
+    for _ in range(n):
+        delays.append(asyncio.create_task(wait_random(max_delay)))
 
-    return results
+    completed = []
+    for task in asyncio.as_completed(delays):
+        delay = await task
+        completed.append(delay)
+
+    return completed
