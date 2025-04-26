794c2c4859ba025d7a4a1f51ca05e45b5eb6e77c
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-26 14:53:06 +0100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function sum_mixed_list which returns
+the sum of a list containing integers and floats.
+"""
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of all integers and floats in mxd_lst as a float."""
+    return sum(mxd_lst)
