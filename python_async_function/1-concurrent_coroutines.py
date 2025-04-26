545af0576f650a2065d555a3c92b061b3916b9d8
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-26 15:16:05 +0100

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module that defines an async routine to spawn multiple wait_random coroutines."""
+"""Module that defines an async routine"""
 
 import asyncio
 from typing import List
@@ -7,7 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay and returns the list of all delays in ascending order."""
+    """Spawns wait_random n times with the specified max_delay and
+    returns the listof all delays in ascending order."""
     delays = []
     for _ in range(n):
         delays.append(asyncio.create_task(wait_random(max_delay)))
