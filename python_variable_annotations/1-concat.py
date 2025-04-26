d73324ee01102ba481ec6bfe340005e65af6c2a6
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-26 14:16:20 +0100

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module containing a function to concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string
+        str2 (str): The second string
+
+    Returns:
+        str: The concatenated string
+    """
+    return str1 + str2
