cc6b48936cbce4675942dbfbffc2ab0a6997bcee
wwalid18 <agar.io.annonyms@gmail.com>
2025-04-29 12:54:08 +0100

    task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, 
+                     index: int = None, 
+                     page_size: int = 10) -> Dict[str, Any]:
+        """Return deletion-resilient pagination dictionary."""
+        assert isinstance(index, int) and index >= 0
+        indexed_data = self.indexed_dataset()
+        assert index < len(indexed_data)
+
+        data = []
+        current_index = index
+        collected = 0
+
+        while collected < page_size and current_index < len(self.__dataset):
+            item = indexed_data.get(current_index)
+            if item is not None:
+                data.append(item)
+                collected += 1
+            current_index += 1
+
+        return {
+            "index": index,
+            "next_index": current_index,
+            "page_size": len(data),
+            "data": data
+        }
